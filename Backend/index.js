1d51ded52059a4e087445247f739886b8aadffd6
Patelsandesh998 <148585012+Patelsandesh998@users.noreply.github.com>
2025-08-10 00:34:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,30 +10,20 @@ import geminiResponse from "./gemini.js";
 import chalk from "chalk";
 
 const app = express();
-
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", 
-      "http://localhost:5174", 
-      "https://smart-virtual-assistant-1.onrender.com" // add your deployed frontend URL here
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
-
-// Enable preflight for all routes
-app.options("*", cors());
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   connectDb();
-  console.log(chalk.green.bold(`Server started on port ${port}`));
+  console.log(chalk.green.bold(" Server started on port 8000"));
 });
